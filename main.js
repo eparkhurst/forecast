bb8978da8f50fa74e93b850febaa5f45c0818aad
eparkhurst <eli.parkhurst@gmail.com>
2016-09-20 21:48:49 -0600

    geolocation is retrieved and saved in session storage

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log('hit');
+//d03abe95d979d9a08ace2336a67520c4
+
+$( "body" ).append( "<p>Test</p>" );
+
+if (sessionStorage.getItem('location')) {
+  var geolocation = JSON.parse(sessionStorage.getItem('location'))
+  getWeatherData(geolocation.lat, geolocation.lng)
+}else{
+  getLocation()
+}
+
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          getWeatherData(position.coords.latitude, position.coords.longitude)
+          var loc = {
+            lat:position.coords.latitude,
+            lng:position.coords.longitude
+          }
+          sessionStorage.setItem('location', JSON.stringify(loc));
+          console.log(position.coords);
+        });
+    } else {
+      console.log('No geolocation');
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+
+function getWeatherData(lat, long){
+  $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"&APPID=d03abe95d979d9a08ace2336a67520c4",
+   function(data){
+    console.log(data.list);
+  })
+}
